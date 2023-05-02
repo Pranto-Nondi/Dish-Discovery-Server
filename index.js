5ca3b3754b4d1dd558279aabb1375ed0988b83b9
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-05-02 17:58:33 +0600

    updated on server

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
-
+const chefs = require('./Data/chefs.json')
 app.use(cors())
 app.get('/', (req, res) => {
     res.send(`chef recipe is running`)
 })
-
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+})
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id
+    const selectedChef = chefs.find(e => e.id == id)
+    res.send(selectedChef)
+})
 
 app.listen(port, () => {
     console.log(`chef recipe is running on Port:${port}`)
